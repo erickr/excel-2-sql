aea2dfb8ed9e3d90d4d238278cf8db26a3dc5d96
Eric Krona <eric@krona.tm>
2018-10-31 21:16:01 +0100

    syntax fix

--- test/excel-2-sql-cli-test.js
+++ test/excel-2-sql-cli-test.js
@@ -1,15 +1,18 @@
-let assert = require('assert');
-let exec = require('child_process');
-let fs = require('fs');
+let assert = require("assert");
+let exec = require("child_process");
+let fs = require("fs");
 
-
-describe('excel-2-sql', function() {
-    describe('insert', function() {
-        it('expected ./excel-2-sql example.xslx example to be the same as test/expected-output/insert-default-options.output', function() {
-            let expectedOutput = fs.readFileSync('./test/expected-output/insert-default-options.output', 'utf8');
-            result = exec.execSync('./excel-2-sql example.xlsx example', {'encoding': 'utf8'});
-            assert.equal(result, expectedOutput);
-
-        });
+describe("excel-2-sql", function() {
+  describe("insert", function() {
+    it("expected ./excel-2-sql example.xslx example to be the same as test/expected-output/insert-default-options.output", function() {
+      let expectedOutput = fs.readFileSync(
+        "./test/expected-output/insert-default-options.output",
+        "utf8"
+      );
+      result = exec.execSync("./excel-2-sql example.xlsx example", {
+        encoding: "utf8"
+      });
+      assert.equal(result, expectedOutput);
     });
+  });
 });
